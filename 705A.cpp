cc8d1c8d41ecac8cc85267dc027a20df3057c276
Niloy Rahman <havocisgod@gmail.com>
2018-12-04 20:57:08 +0600

    Create 705A.cpp

---
+++ 705A.cpp
@@ -0,0 +1,35 @@
+//https://codeforces.com/problemset/problem/705/A
+/*
+@author havoc
+*/
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main () {
+
+
+int n;
+cin>>n;
+for(int i=1;i<=n;i++){
+    if(i!=n){
+        if(i%2==0){
+            cout<<"I love that ";
+        }
+        else cout<<"I hate that ";
+    }
+    else if( i == n){
+        if(n%2==0){
+            cout<<"I love it";
+        }
+        else cout<<"I hate it";
+    }
+
+}
+
+
+
+
+}
