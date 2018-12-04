450679f65160eefc5640a31abcb8c13a6f9e1d82
Niloy Rahman <havocisgod@gmail.com>
2018-12-04 22:19:03 +0600

    Create 867A.cpp

---
+++ 867A.cpp
@@ -0,0 +1,45 @@
+//https://codeforces.com/problemset/problem/867/A
+
+
+#include<bits/stdc++.h>
+
+using namespace std;
+int main () {
+int x;
+cin>>x;
+
+
+std::string s(x, 'a');
+for(int i=0;i<x;i++){
+    cin>>s[i];
+}
+char lastplace;
+char now;
+int fsan=0;
+int fsea=0;
+lastplace=s.at(0);
+
+for(int i=0;i<x;i++){
+    now = s.at(i);
+    if(lastplace == now){
+        continue;
+    }
+    if(lastplace != now){
+        if(lastplace == 'S'){
+            fsea++;
+        }
+        else fsan++;
+    }
+    lastplace=s.at(i);
+
+
+
+}
+if(fsea<=fsan) cout<<"NO";
+else cout<<"YES";
+}
+
+
+
+
+
