1b711c25afcf2e42110bc342a4f6cac78dbaffaa
Niloy Rahman <havocisgod@gmail.com>
2018-12-05 01:19:47 +0600

    Create 959A.cpp

---
+++ 959A.cpp
@@ -0,0 +1,16 @@
+//https://codeforces.com/problemset/problem/959/A
+#include<bits/stdc++.h>
+
+/* @author havoc
+   submitted to codeforces*/
+
+
+   using namespace std;
+
+   int main() {
+    int n;
+    cin>>n;
+
+    (n%2==0)?cout<<"Mahmoud":cout<<"Ehab";
+
+   }
