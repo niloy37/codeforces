f68f43796d3420f53a45687696c73c702cc17d22
Niloy Rahman <havocisgod@gmail.com>
2018-12-05 17:57:09 +0600

    Create 513A.cpp

---
+++ 513A.cpp
@@ -0,0 +1,19 @@
+//https://codeforces.com/problemset/problem/513/A
+
+//by havoccboi, contest: Rockethon 2015, problem: (A) Game, Accepted, #
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main () {
+int a,b,m,n;
+
+cin>>a>>b>>m>>n;
+
+
+(a<=b)?cout<<"Second":cout<<"First";
+
+
+}
