56245ce21a65c50c670c8eb8599b2c7b2e0ce3eb
Niloy Rahman <havocisgod@gmail.com>
2019-03-19 00:34:44 +0600

    Create 617A-Elephant.cpp

---
+++ 617A-Elephant.cpp
@@ -0,0 +1,14 @@
+//https://codeforces.com/problemset/problem/617/A
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+int n;
+int count=0;
+
+while(cin>>n){
+    count = n/5;
+    if(n%5!= 0)
+        count++;
+    cout<<count<<endl;
+  }
+}
