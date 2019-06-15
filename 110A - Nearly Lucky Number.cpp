c2e7d4ee1a270b45211e666d456cc890b43928ba
Niloy Rahman <havocisgod@gmail.com>
2019-06-15 22:48:52 +0600

    Create 110A - Nearly Lucky Number.cpp

---
+++ 110A - Nearly Lucky Number.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+string s;
+int luckyCount=0;
+cin>>s;
+for(char a : s){
+    if(a == '4' || a =='7'){
+        luckyCount++;
+    }
+}
+(luckyCount == 4 || luckyCount == 7)?cout<<"YES":cout<<"NO";
+
+}
