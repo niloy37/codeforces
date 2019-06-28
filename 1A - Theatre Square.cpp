ff275bdf81aafc2b77df69645fb98f1ba9b75290
Niloy Rahman <havocisgod@gmail.com>
2019-06-29 00:04:39 +0600

    Create 1A - Theatre Square.cpp

---
+++ 1A - Theatre Square.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main () {
+ long  long int n,m,a;
+
+cin>>n>>m>>a;
+long long int c1 = 0,c2 = 0;
+if(n%a == 0){
+    c1 = n/a;
+}
+else {
+    c1 = n/a;
+    c1++;
+}
+
+if(m%a == 0){
+    c2 = m/a;
+}
+else {
+    c2= m/a;
+    c2++;
+}
+long long int ans = c1*c2;
+cout<<ans<<endl;
+
+}
