c87f606dac9627604111eb34620217c8a728c179
Niloy Rahman <havocisgod@gmail.com>
2019-06-29 21:45:17 +0600

    Create 158A - Next Round.cpp

---
+++ 158A - Next Round.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main () {
+int n,k;
+cin>>n>>k;
+int arr[n];
+
+for(int i=0;i<n;i++) cin>>arr[i];
+int c=0;
+if(arr[k-1]>0){
+for(int z : arr)
+if(z >=arr[k-1])
+        c++;
+}
+
+        else {
+            for(int z : arr)
+            if(z>arr[k-1])
+            c++;
+        }
+        cout<<c<<endl;
+
+}
