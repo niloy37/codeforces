e3708d9677fcf0de2d49c5769df2321ee92ca6e9
Niloy Rahman <havocisgod@gmail.com>
2019-07-04 23:11:55 +0600

    Create 116A - Tram.cpp

---
+++ 116A - Tram.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+	int t;
+	cin>>t;
+	int maxCount=0,count=0;
+	while(t--){
+		int a,b;
+		cin>>a>>b;
+		count-=a;
+		count+=b;
+		if(count> maxCount){
+			maxCount = count;
+		}
+}
+cout<<maxCount<<endl;
+}
