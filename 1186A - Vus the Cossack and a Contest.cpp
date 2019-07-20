15498f63974128002b35ae81ef70fb26c2639fd3
Niloy Rahman <havocisgod@gmail.com>
2019-07-20 20:31:09 +0600

    Create 1186A - Vus the Cossack and a Contest.cpp

---
+++ 1186A - Vus the Cossack and a Contest.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    int n,m,k;
+    while(cin>>n>>m>>k){
+        if(n<= m && n<=k) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+}
