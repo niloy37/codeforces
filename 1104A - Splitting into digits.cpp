ae25655118e17dfeeb2f72af954f1dd193319d2b
Niloy Rahman <havocisgod@gmail.com>
2019-07-22 00:20:25 +0600

    Create 1104A - Splitting into digits.cpp

---
+++ 1104A - Splitting into digits.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+   long long int n;
+   cin>>n;
+   cout<<n;
+   cout<<endl;
+for(long long int i=0;i<n;i++){
+    cout<<1<<" ";
+}
+    cout<<endl;
+    return 0;
+}
