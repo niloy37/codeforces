d67d69951ba68b86e18b4afb404deea8130e4067
Niloy Rahman <havocisgod@gmail.com>
2019-07-22 22:56:26 +0600

    Create 1093A - Dice Rolling.cpp

---
+++ 1093A - Dice Rolling.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int t;
+    cin>>t;
+    int n;
+    while(t--){
+        cin>>n;
+        if(n<=7)cout<<"1"<<endl;
+        else {
+            if(n%7 != 0)
+            cout<<(n/7+1)<<endl;
+            else cout<<(n/7)<<endl;
+        }
+    }
+}
