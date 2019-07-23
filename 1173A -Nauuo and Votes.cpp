697476d575bba2f84812282e580212e041f06c60
Niloy Rahman <havocisgod@gmail.com>
2019-07-23 15:22:00 +0600

    Create 1173A -Nauuo and Votes.cpp

---
+++ 1173A -Nauuo and Votes.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc , char**argv){
+	int x,y,z;
+	cin>>x>>y>>z;
+	int c=0;
+	if(x-y+z >= 0 && x-y-z <= 0 && z !=0){
+		cout<<"?";
+	}
+	else if(x-y+z < 0){
+		cout<<"-";
+	}
+	else if(x-y-z>0){
+		cout<<"+";
+	}
+	else cout<<"0";
+}
