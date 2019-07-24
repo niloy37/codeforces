b467a9a2239513921015f241a7aa887d7b885c3c
Niloy Rahman <havocisgod@gmail.com>
2019-07-24 20:42:08 +0600

    Create Problem A.cpp

---
+++ contest 575/Problem A.cpp
@@ -0,0 +1,16 @@
+/*
+All praises to the Almighty, May Allah give us the strength to come over all our obstacles
+*/
+#include<iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int x,y,z;
+        cin>>x>>y>>z;
+
+        cout<<(x+y+z)/2<<endl;
+    }
+}
