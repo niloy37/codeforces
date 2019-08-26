5c443a4abd104425bc1c92d25c3175bf02dc1fde
Niloy Rahman <havocisgod@gmail.com>
2019-08-27 01:45:18 +0600

    Create 1065A - Vasya and Chocolate.cpp

---
+++ 1065A - Vasya and Chocolate.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main () 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int s,a,b,c;
+        cin>>s>>a>>b>>c;
+        long long int at = s/c;
+        long long int free = at/a;
+        cout<<at+free*b<<endl;
+    }
+}
